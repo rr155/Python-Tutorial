a3504f4a08f603db1f7bb219e0f536c446f27be5
rr155 <mastromaster.gamer@gmail.com>
2024-08-03 14:49:56 +0530

    First Push

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+print('Hello World')
+v1 = 3 
+v2 = 8
+print(type(v1))
+print(v1+v2)
+var3 = [1,2,3]
+print(type(var3))
---
+++ test_file.py
@@ -0,0 +1,6 @@
+print('test')
+var3 = [1,3,4]
+print(type(var3))
+
+for i in var3:
+    print(i)
